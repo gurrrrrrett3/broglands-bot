7b9ec6120167a1cd72c46848544af0085093f56d
Garrett <garrettkeneau@gmail.com>
2022-04-09 12:42:51 -0400

    Add sessions

---
+++ modules/commands/sessions.ts
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder, SlashCommandStringOption } from "@discordjs/builders";
+import Discord from "discord.js";
+import SessionsViewer from "../bot/sessionsViewer";
+import SessionPagedEmbed from "../resources/sessionPagedEmbed";
+
+const Command = {
+  data: new SlashCommandBuilder()
+    .setName("sessions")
+    .setDescription("Get the recent sessions of a player")
+    .addStringOption(
+      new SlashCommandStringOption()
+        .setName("player")
+        .setRequired(true)
+        .setDescription("The player to get the sessions of")
+    ),
+  async execute(interaction: Discord.CommandInteraction, ...args: any[]) {  
+    interaction.reply(SessionsViewer.displayOnCommandInteraction(interaction));
+  },
+};
+module.exports = Command;
